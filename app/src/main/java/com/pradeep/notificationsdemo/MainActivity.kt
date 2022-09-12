2c0f52b9e7eeb9782617ea9bf81e040b459c0673
Pradeep <iampradeephr@gmail.com>
2022-09-13 00:06:20 +0530

    Showing images in Notifications

--- app/src/main/java/com/pradeep/notificationsdemo/MainActivity.kt
+++ app/src/main/java/com/pradeep/notificationsdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pradeep.notificationsdemo.databinding.ActivityMainBinding
+import com.pradeep.notificationsdemo.demo.DemoNotifications
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +19,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             service.showNotification(Counter.value)
+        }
+        binding.buttonDemoNotification.setOnClickListener{
+            val obj=DemoNotifications(applicationContext)
+//            obj.showNotification("Hello there","You can set your username for all repositories at a given site by putting something like the following in your Git configuration file. You'll want to change \"https://example.com\" and \"me\" to the actual URL and your actual username.")
+            obj.showImageNotification("title","body")
         }
     }
 }
--- app/src/main/java/com/pradeep/notificationsdemo/demo/DemoNotifications.kt
+++ app/src/main/java/com/pradeep/notificationsdemo/demo/DemoNotifications.kt
@@ -1,6 +1,97 @@
 package com.pradeep.notificationsdemo.demo
 
-class DemoNotifications {
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+
+import android.graphics.drawable.BitmapDrawable
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.pradeep.notificationsdemo.MainActivity
+import com.pradeep.notificationsdemo.R
+
+class DemoNotifications(private val context: Context) {
+
+    private val notificationManager=context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    fun showNotification(title:String,body:String){
+
+        val intent= Intent(context,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent= PendingIntent.getActivity(
+            context,
+            1,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val notification=NotificationCompat
+            .Builder(context,context.getString(R.string.Demo_Notification_ID))
+            .setSmallIcon(R.drawable.ic_baseline_adb_24)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        notificationManager.notify(
+            1,
+            notification
+        )
+    }
+
+    fun showImageNotification(title:String,body:String){
+
+        val intent= Intent(context,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent= PendingIntent.getActivity(
+            context,
+            2,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+
+
+        val bigPictureBitmap = ContextCompat.getDrawable(context, R.drawable.wojak)?.toBitmap()
+        val bigLargeIconBitmap = ContextCompat.getDrawable(context, R.drawable.wojak)?.toBitmap()
+        val bigPictureStyle = NotificationCompat.BigPictureStyle()
+            .setBigContentTitle("Foo Big Content Title")
+            .setSummaryText("Foo summary text")
+            .bigLargeIcon(bigLargeIconBitmap)
+            .bigPicture(bigPictureBitmap)
+
+
+
+
+
+
+        val notification=NotificationCompat
+            .Builder(context,context.getString(R.string.Demo_Notification_ID))
+            .setSmallIcon(R.drawable.ic_baseline_adb_24)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setStyle(bigPictureStyle)
+            .build()
+
+        notificationManager.notify(
+            2,
+            notification
+        )
+    }
+
 
 
 }
